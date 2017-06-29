5d4a126f8d46d9e9195f9740dcf8d76107203857
dmitry karlinsky <dmitryka@wix.com>
2017-06-29 10:07:27 +0300

    added some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,6 @@
 alias reload!='. ~/.zshrc'
+alias ports='lsof -i -P -sTCP:LISTEN'
+alias psgrep='ps aux|grep'
+alias flushdns='sudo killall -HUP mDNSResponder'
+alias f5-reset='sudo killall -HUP mDNSResponder'
+
