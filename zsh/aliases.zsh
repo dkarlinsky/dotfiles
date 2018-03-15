3561a46c199b59bfa06c7a738ccee49f0022e240
dmitry karlinsky <dmitryka@wix.com>
2018-03-15 10:16:04 +0200

    * lport command * mci, mci-p aliases * teamutils completion * iterm stuff * nvm

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,6 @@ alias ports='lsof -i -P -sTCP:LISTEN'
 alias psgrep='ps aux|grep'
 alias flushdns='sudo killall -v -HUP mDNSResponder'
 alias f5-reset='sudo killall -v -9 svpn'
+alias mci-p='pushd ~/dev/com.github/wix-private/crm-inbox-server/crm-profiles;mvn clean install -Pinboxserver;popd'
+alias mci='pushd ~/dev/com.github/wix-private/crm-inbox-server;mvn clean install;popd'
 
